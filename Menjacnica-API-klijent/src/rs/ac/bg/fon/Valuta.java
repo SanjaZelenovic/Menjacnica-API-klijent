e4f748874c32470555df77efa296d57c710c17ca
SanjaZelenovic <sanja.zelenovic26@gmail.som>
2015-05-22 23:49:07 +0200

    Napravljena klasa Valuta sa privatnim atributima, odgovarajucim get, set, toString() i equals metodama.

---
+++ Menjacnica-API-klijent/src/rs/ac/bg/fon/Valuta.java
@@ -0,0 +1,35 @@
+package rs.ac.bg.fon;
+
+public class Valuta {
+
+	private String naziv;
+	private double kurs;
+	
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	public double getKurs() {
+		return kurs;
+	}
+	public void setKurs(double kurs) {
+		this.kurs = kurs;
+	}
+	
+	public String toString() {
+		return "Valuta [naziv=" + naziv + ", kurs=" + kurs + "]";
+	}
+	
+	public boolean equals(Object obj) {
+		
+		if ( obj instanceof Valuta) {
+			Valuta v = (Valuta) obj;
+			if(v.getNaziv().equals(naziv) && v.getKurs() == kurs)
+			return true;
+		}
+		return false;
+	}
+	
+}
